15ad63f290a31a1158b72a5f7b723af6da33c84f
Nishkarsh Singh <110491126+errorwars@users.noreply.github.com>
2022-12-07 21:55:13 +0530

    Add files via upload

---
+++ function.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void displayAge(int);//here in declaration name is not necessary of variable but data type is must.
+int main()
+{
+    int age;
+    printf("Enter the age\n");
+    scanf("%d",&age);
+    displayAge(age);//here value of age is called arguement.
+    printf("age is %d\n",age);
+    return 0;
+}
+void displayAge(int a)//here the value from arguement came and is called parameter.
+{
+    
+    a=23;
+    printf("a=%d\n",a);
+}
