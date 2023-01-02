398cfe1169ad3467c47642977b547f4ec5295382
Nishkarsh Singh <110491126+errorwars@users.noreply.github.com>
2023-01-02 10:58:03 +0530

    FIBONACCI SERIES

--- EX3.1.c
+++ EX3.1.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+int fibo(int);
+void main()
+{
+  int num;
+  printf("Enter the number of terms you want to print of Fibonacci Series.\n");
+  scanf("%d",num);
+  fibo(num);
+  
+}
 
+int fibo(int num)
+{
+  int a=0;
+  int b=1;
+  int c;
+  printf("%d\n%d\n",a,b);
+  for (int i=2 ; i<= num; i++)
+  {
+    c=a+b;
+    a=b;
+    b=c;
+    printf("%d\n",c);
+  }
