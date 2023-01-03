3145233d31511a883d4471ccef88b981eb61fda5
Nishkarsh Singh <110491126+errorwars@users.noreply.github.com>
2023-01-03 10:37:17 +0530

    Taking inputs from user in arrar.

--- array1.c
+++ array1.c
@@ -1,6 +1,8 @@
+/* This is the program to take 10 inputs in an array
+and print that arry*/
 #include <stdio.h>
 int main(){
-int a[10];
+int a[10]; // declaring array of size 10
 printf("Enter 10 numbers ");
 for(int i=0 ; i<2 ; i++)
     {
