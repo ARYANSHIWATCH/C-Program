91d9b3cb23dcfed91120ce95b7f064dae46c6a92
Nishkarsh Singh <110491126+errorwars@users.noreply.github.com>
2023-01-08 22:07:58 +0530

    Create starPattern1.c

--- starPattern1.c
+++ starPattern1.c
@@ -5,10 +5,10 @@ int main()
     printf("printing pattern\n");
     for(i=1;i!=10;i++)
     {
-    for(j=1;j<=i;j++)
-    {
-        printf("*");
-    }
+        for(j=1;j<=i;j++)
+        {
+            printf("*");
+        }
     printf("\n");
     }
     return 0;
